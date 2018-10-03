59874a000043548ecefaa6ef8ead2bf5a03b9c48
Vlad Shepshuk <closetoftheclub@gmail.com>
2018-10-03 18:19:22 +0300

    added upload button

--- client/components/App.js
+++ client/components/App.js
@@ -1,7 +1,23 @@
 import customHTML from './index.html';
-import style from '../../public/style.css';
+import style from '../../public/style.scss';
 
 let root = document.getElementById('root');
 let ih = root.innerHTML;
 
 root.innerHTML = customHTML + ih;
+
+
+const typeFile = document.getElementsByClassName('cv')[0];
+const uploadBtn = document.getElementsByClassName('custom-upload-btn')[0];
+
+typeFile.addEventListener('change', function(event) {
+  uploadBtn.innerHTML = this.files[0].name;
+});
+
+typeFile.addEventListener('mouseover', function(event) {
+  uploadBtn.style.border = '1px solid #fddc3e';
+})
+
+typeFile.addEventListener('mouseout', function(event) {
+  uploadBtn.style.border = '1px solid #dcdcdc';
+})
