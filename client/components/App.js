7521ec6793e8614482272fc37c0165a885e62aae
Vlad Shepshuk <closetoftheclub@gmail.com>
2018-10-05 10:02:44 +0300

    done

--- client/components/App.js
+++ client/components/App.js
@@ -9,6 +9,7 @@ root.innerHTML = customHTML + ih;
 
 const typeFile = document.getElementsByClassName('cv')[0];
 const uploadBtn = document.getElementsByClassName('custom-upload-btn')[0];
+const input = document.getElementsByTagName('input');
 
 typeFile.addEventListener('change', function(event) {
   uploadBtn.innerHTML = this.files[0].name;
@@ -21,3 +22,11 @@ typeFile.addEventListener('mouseover', function(event) {
 typeFile.addEventListener('mouseout', function(event) {
   uploadBtn.style.border = '1px solid #dcdcdc';
 })
+
+// const f = document.getElementsByClassName('form')[0].elements[0];
+//
+// f.addEventListener('change', function() {
+//   console.log();
+//   console.log(f.checked);
+//
+// });
